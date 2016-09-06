d4fbd9c5c060c24190aca80c1fca42916ae65b51
Fizzo <809050631@qq.com>
2016-09-06 18:16:05 +0800

    xxxx

--- public/javascripts/cashFlowPlant.js
+++ public/javascripts/cashFlowPlant.js
@@ -3,3 +3,15 @@
  */
 
 var cfp = {}
+
+
+cfp.cashIn = [] // 现金流入
+cfp.racf = [] //经营活动净现金流量
+cfp.revenue = [] // 营业收入
+cfp.VATRecognitionTax = [] //增值税销项税额
+cfp.subIncome = [] //补贴收入
+cfp.otherIn = []//其他流入
+cfp.investDifPic = [] //投资价差
+cfp.cashOut = [] //现金流出
+cfp.runCost = []//运营成本
+cfp.inputVAT = [] //增值税进项税额
